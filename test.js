2aadcb3934569199e7a7089772ca68736922f7eb
Emily Miller <esm1503@gmail.com>
2014-06-17 20:54:07 -0600

    changed file content

--- test.js
+++ test.js
@@ -1,6 +1,27 @@
 
-v("'sup")
+var types = {
+    string: "a string",
+    number: 42,
+    boolean: true,
+    array: [1,2,3,4]
+};
 
-function v(val){
-    console.log(val)
+if(types.number === 42){
+    console.log("you found me")
+}
+else {
+    types.boolean === true
+    console.log("you lost me")
+}
+var key;
+
+for(key in types) {
+    console.log(key, types[key])
+    console.log('========')
+}
+
+var i;
+console.log('iterating array')
+for (i=0; i< types.array.length; i++) { //reset i to zero, while i is less than the amount in the array, increments variable
+    console.log(i, types.array[i]);
 }
